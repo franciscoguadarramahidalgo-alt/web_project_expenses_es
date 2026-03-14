9fdc89cba11995eba3038722e220b28a73167c69
FrankGu <francisco.guadarrama.hidalgo@gmail.com>
2026-03-14 03:50:45 -0600

    add the calculations and the colors for prices, need to complete last function

---
+++ scripts/calculations.js
@@ -0,0 +1,80 @@
+let budgetValue = 0; // Importe de presupuesto inicial (Presupuesto)
+let totalExpensesValue = 0; // Valor de la suma total de gastos (Gastos totales)
+
+let expenseEntries = [
+  ["groceries", 33],
+  ["restaurants", 50],
+  ["transport", 12],
+  ["home", 70],
+  ["subscriptions", 14],
+  ["groceries", 28],
+  ["subscriptions", 12],
+]; // Array para almacenar los gastos ingresados
+
+for (let i = 0; i < expenseEntries.length; i++) {
+  totalExpensesValue += expenseEntries[i][1]; //Suma del valor de cada gasto para formar el total
+  console.log("Valor total de los gastos: " + totalExpensesValue); //Revisión de valores en la consola
+}
+
+function calculateAverageExpense() {
+  if (expenseEntries.length === 0) {
+    return 0;
+  } else {
+    return totalExpensesValue / expenseEntries.length;
+  }
+}
+
+function calculateBalance() {
+  return budgetValue - totalExpensesValue;
+}
+
+let balanceColor = "green"; // Color para el balance (verde por defecto)
+
+function updateBalanceColor() {
+  if (calculateBalance() < 0) {
+    balanceColor = "red";
+  } else if (calculateBalance() < budgetValue * 0.25) {
+    balanceColor = "orange";
+  } else {
+    balanceColor = "green";
+  }
+}
+
+function calculateCategoryExpenses(category) {
+  let total = 0;
+  for (let i = 0; i < expenseEntries.length; i++) {
+    if (expenseEntries[i][0] === category) {
+      total += expenseEntries[i][1];
+    }
+  }
+  return total;
+}
+
+let categoriesProgram = [
+  "groceries",
+  "restaurants",
+  "transport",
+  "home",
+  "subscriptions",
+];
+function calculateLargestCategory() {
+  let largestCategory = "";
+  let largestAmount = 0;
+  for (let i = 0; i < categoriesProgram.length; i++) {
+    let categoryExpense = calculateCategoryExpenses(categoriesProgram[i]);
+    if (categoryExpense > largestAmount) {
+      largestAmount = categoryExpense;
+      largestCategory = categoriesProgram[i];
+    }
+  }
+  return largestCategory;
+}
+
+let categoriesData = []; //Array para almacenar los datos de cada categoria y su balance más alto
+
+for (let i = 0; i < categoriesProgram.length; i++) {
+  categoriesData.push({
+    category: categoriesProgram[i],
+    total: calculateCategoryExpenses(categoriesProgram[i]),
+  });
+}
